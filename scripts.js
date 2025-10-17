d065b0db3c09f2d4a823f815cc9a7bb2b6a8e96e
gokdenizuzunoglu <88375162+gokdenizuzunoglu@users.noreply.github.com>
2025-10-17 14:51:31 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 320;
 canvas.height = 320;
+
+ctx.fillStyle = "red";
+ctx.begin();
+ctx.arc(160, 160, 100, 0, 2*Math.PI);
+ctx.fill();
+
+ctx.fillStyle = "white";
+ctx.fillText("BM101 Lab'a Selamlar!", 80, 150);
